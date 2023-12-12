c0c3923305088be035aa488f4d0deba91c51dcdf
vargbalazs <balazs.varga.8564@gmail.com>
2023-12-12 14:12:44 +0100

    d5t2

---
+++ day5/task-2-solution.ts
@@ -0,0 +1,19 @@
+const sds: string = "79 14 55 13";
+
+const sdToSl: string[] = ["50 98 2", "52 50 48"];
+
+const slToFert: string[] = ["0 15 37", "37 52 2", "39 0 15"];
+
+const fertToWat: string[] = ["49 53 8", "0 11 42", "42 0 7", "57 7 4"];
+
+const watToLight: string[] = ["88 18 7", "18 25 70"];
+
+const ligthToTemp: string[] = ["45 77 23", "81 45 19", "68 64 13"];
+
+const tempToHum: string[] = ["0 69 1", "1 0 69"];
+
+const humToLoc: string[] = ["60 56 37", "56 93 4"];
+
+function solved5t2(): number {
+  return 0;
+}
